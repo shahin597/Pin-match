9c387474c6f7393613fb7943c70323a722dfc0c6
shahin597 <shahincse62@gmail.com>
2022-02-16 21:58:21 +0600

    done

--- js/index.js
+++ js/index.js
@@ -15,3 +15,38 @@ function generatePin() {
     const pin = getPin();
     document.getElementById('display-pin').value = pin;
 }
+
+// (event) called event bubble
+document.getElementById('key-pad').addEventListener('click', function (event) {
+    const number = event.target.innerText;
+    const calcInput = document.getElementById("typed-numbers");
+    if (isNaN(number)) {
+        if (number == 'C') {
+            calcInput.value = ''; //c diye clear korbe
+        }
+    }
+    else {
+        const previousNumber = calcInput.value;
+        const newNumber = previousNumber + number;
+        calcInput.value = newNumber;
+
+    }
+});
+
+function verifyPin() {
+    const pin = document.getElementById('display-pin').value;
+    const typedNumbers = document.getElementById('typed-numbers').value;
+    const successMessage = document.getElementById('notify-success');
+    const failError = document.getElementById('notify-fail');
+
+    if (pin == typedNumbers) {
+
+        successMessage.style.display = 'block';
+        failError.style.display = 'none';
+    }
+    else {
+
+        failError.style.display = 'block';
+        successMessage.style.display = 'none';
+    }
+}
